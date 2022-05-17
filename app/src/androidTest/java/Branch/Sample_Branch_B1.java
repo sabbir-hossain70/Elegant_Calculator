377041193045f166a7fe4c6fc816e101367b0b96
Sabbir <ak47.sabbir.bd@gmail.com>
2022-05-18 00:30:18 +0600

    Modified sample branch b1.java from master

--- app/src/androidTest/java/Branch/Sample_Branch_B1.java
+++ app/src/androidTest/java/Branch/Sample_Branch_B1.java
@@ -3,10 +3,21 @@ package Branch;
 public class Sample_Branch_B1 {
     String name,id;
     float cgpa;
-    Sample_Branch_B1(String s,String s1,float f){
+    int number_of_classes;
+
+    public void setNumber_of_classes(int number_of_classes) {
+        this.number_of_classes = number_of_classes;
+    }
+
+    public int getNumber_of_classes() {
+        return number_of_classes;
+    }
+
+    Sample_Branch_B1(String s, String s1, float f, int cls){
         this.name=s;
         this.id=s1;
         this.cgpa=f;
+        this.number_of_classes=cls;
     }
 
     public String getName() {
